52f3c1edd1fe7fe21d9b8ea6bd33c98fa933a62b
Patrick <patrick.c.mcpartland@gmail.com>
2019-01-24 04:57:09 -0700

    servey fix

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,7 +3,7 @@ var path = require("path");
 module.exports = function(app) {
     //Route to landing/index.html
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
     //Route to survey/survey.html
